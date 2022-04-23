d293b5065aaa921a9c78fd5e87dc55a5fcdfde9f
Gabriela Leite <nleite.gabriela@gmail.com>
2022-04-23 17:43:58 -0300

    challenge, without css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do |_i|
+  p Article.create(title: Faker::Movie.title, content: Faker::Movie.quote)
+end
